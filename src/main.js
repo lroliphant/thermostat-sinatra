e41af8d30488c55b85fbe0f2bb8fe7eb0ca72436
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-07 12:30:41 +0100

    add up and down buttons to change the temperature - UI

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 var thermostat = new Thermostat();
 var currentTemp = thermostat.temperature;
-document.getElementById('currentTemp').innerHTML = currentTemp;
+document.getElementById('temperature').innerHTML = currentTemp;
+
+function upTemp() {
+  thermostat.increase();
+  currentTemp = thermostat.temperature;
+  document.getElementById('temperature').innerHTML = currentTemp;
+}
+
+function downTemp() {
+  thermostat.decrease();
+  currentTemp = thermostat.temperature;
+  document.getElementById('temperature').innerHTML = currentTemp;
+}
+
+
+
+//
+// document.getElementById("button-up").onclick = upTemp;
