c2dfb7ae2462903e339c4cbd703ccddaacb8ed88
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-07 13:27:06 +0100

    Add thermostat colour change

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ function upTemp() {
   thermostat.increase();
   currentTemp = thermostat.temperature;
   document.getElementById('temperature').innerHTML = currentTemp;
+  changeColour();
 }
 
 function downTemp() {
   thermostat.decrease();
   currentTemp = thermostat.temperature;
   document.getElementById('temperature').innerHTML = currentTemp;
+  changeColour();
 }
 
 function togglePowerSaver() {
@@ -22,6 +24,18 @@ function reset() {
   thermostat.resetTemp();
   currentTemp = thermostat.temperature;
   document.getElementById('temperature').innerHTML = currentTemp;
+}
+
+function changeColour() {
+  if (thermostat.energyUse() === 'low') {
+    document.getElementById('temperature').className = 'low';
+  }
+  else if (thermostat.energyUse() === 'high') {
+    document.getElementById('temperature').className = 'high';
+  }
+  else {
+    document.getElementById('temperature').className = 'medium';
+  };
 }
 
 
